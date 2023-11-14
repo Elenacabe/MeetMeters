611105ee33335e559d9b4f4ca0a1da0b64f3550f
angel060404 <angel.s.p2064@gmail.com>
2023-11-14 15:27:04 +0100

    search by title added

--- routes/gallery.routes.js
+++ routes/gallery.routes.js
@@ -3,12 +3,19 @@ const router = express.Router();
 // const galleryController = require("../controllers/gallery.controller");
 const GalleryService = require('../services/gallery.services');
 // const gallery = require("../models/gallery.model")
-
+function isValidGalleryId(galleryId) {
+    return GalleryService.findOneOfGalleryById(galleryId)
+        .then(response => response.status === 200)
+        .catch(() => false);
+}
 /* GET home page */
 // router.get("/", galleryController.search)
 //router.get("/author", galleryController.author);
 // router.get("/details/:_id", galleryController.id)
-router.get("/", (req, res, next) => {
+router.get("/"), (req, res, next) => {
+    res.render('Gallery/galleryList')
+}
+router.get("/search", (req, res, next) => {
     const { search } = req.query
 
 
