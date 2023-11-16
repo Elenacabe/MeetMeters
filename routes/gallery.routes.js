4603479e57e1f696803d4b11c026b3fd9b63ead0
Elena Ceinos <elenaceinos@hotmail.com>
2023-11-16 16:47:31 +0100

    save it

--- routes/gallery.routes.js
+++ routes/gallery.routes.js
@@ -2,18 +2,10 @@ const express = require('express')
 const router = express.Router()
 // const galleryController = require("../controllers/gallery.controller")
 const GalleryService = require('../services/gallery.services')
-const isValidGalleryId = require('../utils/validId')
 const User = require('../models/User.model')
 const userFavorities = require('../utils/favorites')
 
 
-// const gallery = require("../models/gallery.model")
-
-
-/* GET home page */
-// router.get("/", galleryController.search)
-//router.get("/author", galleryController.author)
-// router.get("/details/:_id", galleryController.id)
 
 router.get("/", (req, res, next) => {
     res.render('Gallery/galleryList')
@@ -36,13 +28,13 @@ router.get('/details/:objectID', (req, res, next) => {
         .catch(err => next(err))
 })
 
-router.post('/favorites/:art_id', (req, res, next) => {
-    const { art_id } = req.params
+router.post('/favorites/:_artId', (req, res, next) => {
+    const { _artId } = req.params
     const currentUser = req.session.currentUser._id
     User
         .findById(currentUser)
-        .then(user => userFavorities(user, art_id))
-        .then(() => res.redirect('/gallery'))
+        .then(user => userFavorities(user, _artId))
+        .then(() => res.redirect('/'))
         .catch(err => next(err))
 })
 
